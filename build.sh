41d89d729f61f7eab25b4cb76d68700b50ea3d20
Robin Appelman <robin@icewind.nl>
2022-03-09 23:04:00 +0100

    initial version

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d php ]; then
+  git clone https://github.com/docker-library/php
+fi
+
+patch -N -p1 -d php < debug.patch
+patch -N -p1 -d php < version.patch
+
+versions=("7.3" "7.4" "8.0" "8.1")
+
+cd php; ./apply-templates.sh
+
+for version in "${versions[@]}"; do
+  docker build -t icewind1991/php-debug:$version-fpm -f php/$version/bullseye/fpm/Dockerfile php/$version/bullseye/fpm
+done
